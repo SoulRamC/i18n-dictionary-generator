99ecedc4b166dcd9a494b60802c28c498254fcfa
SoulRamC <samiramzi2000@gmail.com>
2024-11-16 22:24:10 +0100

    feat: adapting jsDocs to the commands

--- src/commands/generate_default_translation_dictionary.js
+++ src/commands/generate_default_translation_dictionary.js
@@ -1,8 +1,14 @@
 import { GenerateDefaultTranslationDictionary } from '../utils/create_default_dictionary.js';
 
+/**
+ * Adds the `generate` command to the Commander.js program.
+ *
+ * @param {import('commander').Command} program - The Commander.js program instance to which the command will be added.
+ * @returns {void}
+ */
 export default (program) => {
   program
-    .command('gd')
+    .command('generate')
     .description('Generate default translation dictionary named en.json')
     .action(async () => {
       await GenerateDefaultTranslationDictionary();
